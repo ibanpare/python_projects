2a62b62f27fce701b943c57b49d30397c9fd86ae
ibanpare <parenti.ivan@gmail.com>
2020-11-07 21:59:45 +0100

    Added files

---
+++ atomic_time.py
@@ -0,0 +1,4 @@
+"""
+This program will get the true atomic time from an atomic time clock on the Internet. 
+Use any one of the atomic clocks returned by a simple Google search.
+"""
---
+++ recipe_creator_and_manager.py
@@ -0,0 +1,5 @@
+"""
+Create a recipe class with ingredients and a put them in a recipe manager program 
+that organizes them into categories like desserts, main courses 
+or by ingredients like chicken, beef, soups, pies etc.
+"""
