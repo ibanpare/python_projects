a40b586ee8c52d414850e3944933a9155a198750
ibanpare <parenti.ivan@gmail.com>
2020-11-08 13:47:26 +0100

    Worked on atomic clock and added a similar version

--- atomic_time.py
+++ atomic_time.py
@@ -1,4 +1,12 @@
 """
-This program will get the true atomic time from an atomic time clock on the Internet. 
+This program will get the true atomic time from an atomic time clock on the Internet.
 Use any one of the atomic clocks returned by a simple Google search.
 """
+
+import requests
+import bs4
+
+res = requests.get("https://www.timeanddate.com/worldclock/other/tai")
+soup = bs4.BeautifulSoup(res.text,"lxml")
+time = soup.select("#ct")
+print(f"The atomic time is now: {time[0].text}")
