1c5db021a27d50b6348a083932bc8de4fb9baa66
ibanpare <parenti.ivan@gmail.com>
2020-11-24 08:20:14 +0100

    added a future project

---
+++ web/university_schedule_scraping.py
@@ -0,0 +1,7 @@
+"""You will develop a Python program to dynamically complete certain tasks,
+ such as list, find, sort, and save, in course listings from the scheduling portal.
+ You will mainly use "request" and "BeautifulSoup" libraries (or similar, see exercise 12.1).
+ The program will operate at a different level: Semester and Department.
+ Your program will be a menu-based application. Assume that your project file is myproject.py.
+ Once you run, it will show the last 5 semester
+ (fall, spring, summer-only, (not wintersemester or, may mini semester))"""
