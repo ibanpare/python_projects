42f61c8aa72521ff586941086c4df4822ec9a1af
ibanpare <parenti.ivan@gmail.com>
2020-12-22 18:55:51 +0100

    more work

--- web/university_schedule_scraper/university_schedule_scraping.py
+++ web/university_schedule_scraper/university_schedule_scraping.py
@@ -7,21 +7,22 @@
  (fall, spring, summer-only, (not wintersemester or, may mini semester))"""
 
 import bs4
+from collections import namedtuple
 import requests
 import time
 
 BASE_URL = "http://appsprod.tamuc.edu/Schedule/Schedule.aspx"
-TERM_1_URL = "http://appsprod.tamuc.edu/Schedule/Schedule.aspx?Term=202120"
-TERM_2_URL = "http://appsprod.tamuc.edu/Schedule/Schedule.aspx?Term=202110"
-TERM_3_URL = "http://appsprod.tamuc.edu/Schedule/Schedule.aspx?Term=202080"
-TERM_4_URL = "http://appsprod.tamuc.edu/Schedule/Schedule.aspx?Term=202070"
-TERM_5_URL = "http://appsprod.tamuc.edu/Schedule/Schedule.aspx?Term=202050"
 
 home_res = requests.get(BASE_URL)
 home_soup = bs4.BeautifulSoup(home_res.content, "lxml")
-
 semesters = home_soup.find_all("option")
 available_semesters = semesters[0:5]
+
+SEMESTER_1 = {f"{semesters[0].text}": f"{BASE_URL}?Term=202120"}
+SEMESTER_2 = {f"{semesters[0].text}": f"{BASE_URL}?Term=202110"}
+SEMESTER_3 = {f"{semesters[0].text}": f"{BASE_URL}?Term=202080"}
+SEMESTER_4 = {f"{semesters[0].text}": f"{BASE_URL}?Term=202070"}
+SEMESTER_5 = {f"{semesters[0].text}": f"{BASE_URL}?Term=202050"}
 
 print("The available semesters are:")
 for semester in available_semesters:
@@ -34,9 +35,40 @@ class DepartmentSelect():
         time.sleep(2)
         self.soup = bs4.BeautifulSoup(self.res.content, "lxml")
         self.rows = self.soup.find_all("a", class_="nav")
+        self.Department = namedtuple("Department", [
+            "Code",
+            "Name",
+            "Prefixes",
+            ])
+        self.departments = []
+        i = 0
+        while i <= len(self.rows) - 2:
+            self.departments.append(self.Department(self.rows[i].text,
+                self.rows[i + 1].text, self.rows[i + 2].text))
+            i += 3
+        #che poi fare tutta sta roba all'init booh, forse ci sta, il resto però
+        #penso meglio con metodi
 
 #this is for testing
 summer2 = DepartmentSelect( "http://appsprod.tamuc.edu/Schedule/Schedule.aspx?Term=202050")
 
-#to do: siamo arrivati ad avere una lista di dipartimenti per semestre, #bisogna stamparla bene e parsare la pagina corsi per ogni dipartimento.
+print("Here are summer2, which should need a way to know its name, departments")
+for key, dep in enumerate(summer2.departments): print(dep[1])
+
+#bisogna parsare la pagina corsi per ogni dipartimento.
 #attenzione anche a cosa dovranno fare sti corsi per capire come salvarli
+
+
+"""
+Prefix   ID  Sec Name    Instructor  Hours   Seats   Enroll.
+COSC    1301    01W Intro to Compu  Lee, Kwang  3   35  10
+COSC    1436    01E Intro to Comp Sci & Prog    Brown, Thomas   4   40  36
+COSC    1436    01L Intro to Comp Sci & Prog    Brown, Thomas       40  36
+COSC    1436    01W Intro to Comp Sci & Prog    Hu, Kaoning 4   45  43
+COSC    1436    02E Intro to Comp Sci & Prog    Hu, Kaoning 4   35  32
+as first 5 rows.
+
+il prefix è il department
+ci sono vari id per department
+varie sezioni per id
+le hour sono nel campo del name, che gioia (regex)"""
