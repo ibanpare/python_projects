630962bd906cd11d1b1756b6321c0919c63b0df9
ibanpare <parenti.ivan@gmail.com>
2021-01-18 21:01:59 +0100

    more work

--- web/project/project.py
+++ web/project/project.py
@@ -1,6 +1,17 @@
 import requests
 import bs4
 
+"""
+City CAN TAKE FROM QUERY
+Neighborhood NA
+Zipcode  CAN TAKE FROM QUERY
+# of Baths NA IN SEARCH
+Sqft NA IN SEARCH
+
+"""
+
+#headers needed to avoid blocking
+
 headers = {
     'authority': 'www.apartments.com',
     'dnt': '1',
@@ -14,10 +25,38 @@ headers = {
     'accept-language': 'en-GB,en-US;q=0.9,en;q=0.8',
 }
 
-BASE_URL = "https://www.apartments.com/"
 
-res = requests.get(BASE_URL,  headers=headers)
-print(res.status_code)
+def query_url():
+    """
+    query takes:
+    'town-state' will convert even if spaces instead of dashes and uppercase
+    will take ny neighbourhoods
+    'town-state-zipcode' e.g. new-york-ny-10001
+    """
+    home_url = "https://www.apartments.com/"
+    town = input("Insert your town: ")
+    state = input("Insert your state, e.g. CA for California: ")
+    while True:
+        alsozip = input("Do you want to add a ZIP code? (y or n) ")
+        if alsozip.lower() == "y":
+            zipcode = input("Type your ZIP code: ")
+            full_url = home_url + f"{town}-{state}-{zipcode}"
+            return full_url
+        elif alsozip.lower() == "n":
+            full_url = home_url + f"{town}-{state}"
+            return full_url
+        else:
+            continue
+
+res = requests.get(query_url(),  headers=headers)
 soup = bs4.BeautifulSoup(res.content, "lxml")
 
+print(res.status_code)
 print(soup.title)
+
+apartment_name = soup.find(class_="js-placardTitle title")
+apartment_address = soup.find(class_="property-address js-url")
+number_of_beds = soup.find(class_="bed-range")
+rent = soup.find(class_="price-range")
+amenities = soup.find(class_="property-amenities")
+apartment_url = soup.find("a", class_="property-link")
